f9ad33d09ef3a33e2e08b15b0de2592dd6d0fc62
alpbattaloglu <ahmetalpb@gmail.com>
2025-02-22 20:05:03 +0300

    İlk commit: Java String birleştirme örneği eklendi

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, WorldALP!");
+        // var kullanımı
+        var text1 = "Full Stack ";
+        var text2 = "Web Developer Programı";
+        var combinedText = text1 + text2;
+        
+        // Ekrana yazdırma
+        System.out.println(combinedText);
     }
 }
