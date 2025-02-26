ad3345fa0f972f3cc9bcf2d3317dbb57932c1c07
alpbattaloglu <ahmetalpb@gmail.com>
2025-02-26 20:41:20 +0300

    Kod temizlendi

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        // var kullanımı
-        var text1 = "Full Stack ";
-        var text2 = "Web Developer Programı";
-        var combinedText = text1 + text2;
         
-        // Ekrana yazdırma
-        System.out.println(combinedText);
     }
 }
